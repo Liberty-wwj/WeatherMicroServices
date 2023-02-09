7f5e1585162aafab8440f23793d3435c251a2aa7
万文俊 <wenjunwan@mowenjundeMacBook-Pro.local>
2023-02-09 14:31:11 -0800

    2/9

--- NameToZipcode/app.py
+++ NameToZipcode/app.py
@@ -1,29 +1,38 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 import requests
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return '''
-        <form method="post">
-            City in USA: <input type="text" name="city">
-            State in USA: <input type="text" name="state">
-            <input type="submit" value="Submit">
-        </form>
-    '''
-
-@app.route('/', methods=['POST'])
-def get_zipcode():
-    city = request.form['city']
-    state = request.form['state']
-    result = requests.get(f'https://api.zippopotam.us/us/{state}/{city}').json()
-    # https://api.zippopotam.us/us/ca/milpitas
+@app.route("/zipcode/<state_name>/<city_name>")
+def get_zip_code(state_name, city_name):
+    result = requests.get(f'https://api.zippopotam.us/us/{state_name}/{city_name}').json()
     zip_codes = [place['post code'] for place in result['places']]
-    try:
-        return f"Zipcode for {city}: {zip_codes[0]}"
-    except KeyError:
-        return f"No zipcode found for {city}"
+    zip_code = zip_codes[0]
+    # f"http://127.0.0.1:8600/weather/95035"
+    weather_url = f"http://172.22.0.5:8600/weather/{zip_code}"
+    weather_res = requests.get(weather_url)
+    if weather_res.status_code == 200:
+        return f"{weather_res.text}"
+    else:
+        return f"Request failed with status code: {weather_res.status_code}"
+     
+    #try:
+    #    return f"{zip_codes[0]}"
+    #except KeyError:
+    #    return f"No zipcode found for {city_name}"
+    # Use a free API like the OpenCage API to convert city name to zip code
+    #API_KEY = "0179fc6cf2ac46d6b4a9f43c6ab2a8bb"
+    #API_URL = f"https://api.opencagedata.com/geocode/v1/json?q={city_name}&key={API_KEY}"
+    #response = requests.get(API_URL)
+    #data = response.json()
+    
+    # If there are no results for the given city name, return an error message
+    #if len(data["results"]) == 0:
+    #    return jsonify({"error": "No results found for the given city name"}), 400
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    # Extract the zip code from the first result
+    #zip_code = data["results"]
+    #return jsonify({"zip_code": zip_code})
+
+if __name__ == "__main__":
+    app.run(debug=True, port="5000")
---
+++ ZipToWeather/app.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, jsonify
+import requests
+
+app = Flask(__name__)
+
+@app.route("/weather/<zip_code>")
+def get_weather(zip_code):
+    #zip_code = request.form['zipcode']
+    api_key = "83127daeecb54dcc8a2220846230202"
+    weather_url = f"http://api.weatherapi.com/v1/current.json?key={api_key}&q={zip_code}"
+    weather_data = requests.get(weather_url).json()
+#http://api.weatherapi.com/v1/current.json?key=83127daeecb54dcc8a2220846230202&q=95035
+    if "error" in weather_data:
+        return jsonify({"error": weather_data["error"]}), 400
+
+    location = weather_data["location"]
+    current = weather_data["current"]
+
+    return jsonify({
+        "location": location["name"],
+        "region": location["region"],
+        "country": location["country"],
+        "temp_c": current["temp_c"],
+        "condition": current["condition"]["text"],
+        "icon": current["condition"]["icon"],
+    })
+    
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
